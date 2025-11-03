a5c38ac325759cd48c8c3987813d2a6e9de638f4
Prashik Kamble <prashikkamble808@gmail.com>
2025-11-03 12:28:05 +0530

    customized cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,9 @@ async function bootstrap() {
     res.removeHeader('date');
     next();
   });
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
